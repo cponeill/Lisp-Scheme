85806dbc845626812fad69082795f96e056bb02a
cponeill <caseyoneill78@hotmail.com>
2014-02-28 18:33:57 -0800

    Update newton.scm

--- newton.scm
+++ newton.scm
@@ -1,13 +1,10 @@
 This is Newtons Method for Cube Roots
 
-(define (square y)
-  (* y y))
-  
 (define (cube x)
   (/ x 3))
   
-(define (cube-top x)
-  (+ (/ x square) (* square 2)))
+(define (cube-top x y)
+  (+ (/ x y) (* y 2)))
   
 (define (cube-answer x)
   (/ (cube-top cube))
