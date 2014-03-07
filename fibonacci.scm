ba9080d151a75d4cadd2ac54cc72f5cfd1e66cb2
cponeill <caseyoneill78@hotmail.com>
2014-03-07 14:36:05 -0800

    Create fibonacci.scm

---
+++ fibonacci.scm
@@ -0,0 +1,9 @@
+;; Here is the fibonacci sequence written out with Scheme
+
+(define fibonacci
+  (lambda (n)
+    (cond
+      ((= n 0) 0)
+      ((= n 1) 1)
+      (else (+ (fib (- n 1))
+               (fib (- n 1))))))
