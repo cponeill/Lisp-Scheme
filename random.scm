5694f5f7394b9d28c182cc02bff6115df1ef1cb0
cponeill <caseyoneill78@hotmail.com>
2014-03-21 14:30:30 -0700

    Create random.scm

---
+++ random.scm
@@ -0,0 +1,9 @@
+; This is just random Scheme/Lisp code that I am writing as practice. Some of it will be pretty simple and I would like
+; to write longer and more complicated algorithms as I understand this language more thoroughly.
+
+(define area
+  (lambda (length width)
+    (* area width)))
+    
+=> (area 3 4)
+=> 12
