bf2a3e742b8b3e202ef1b058e0d4515e4d2da84e
cponeill <caseyoneill78@hotmail.com>
2014-03-27 14:52:02 -0700

    Update random.scm

--- random.scm
+++ random.scm
@@ -29,3 +29,12 @@
     (lambda (m) (m x y)))
 (define (car z)
     (z (lambda (p w) p)))
+    
+;; More Randomness
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))))
+(define odds (list 1 3 5 7))
+(length odds)
+=> 4
