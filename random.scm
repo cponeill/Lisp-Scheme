26f0eafc32a89b3dcd12d90b3e38fdcb55b9ebee
cponeill <caseyoneill78@hotmail.com>
2014-04-03 18:37:21 -0700

    Update random.scm

--- random.scm
+++ random.scm
@@ -71,3 +71,16 @@
     
 
 
+;; Even more randomness
+(define first
+  (lambda (l)
+    (apply (lambda (x . y) x)
+      (l)))
+(define rest
+  (lambda (l)
+    (apply (lambda (x . y) l)))
+    
+(first '(a b c d))
+=> 'a
+(rest '(a b c d))
+=> '(b c d)
